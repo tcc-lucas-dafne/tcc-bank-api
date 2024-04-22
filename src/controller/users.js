cc766100d213bb5c1e36f172a826830b9953c95d
lcsAndrade <lucascandradex@protonmail.com>
2024-04-21 22:02:19 -0300

    fix: body validation

--- src/controller/users.js
+++ src/controller/users.js
@@ -14,6 +14,8 @@ const pool = new Pool({
 
 const register = (req, res) => {
   const { name, email, password } = req.body;
+
+  if (!name || !email || !password) return res.status(400).json({ "error": "invalid body" })
   
   const hashedPassword = sha1(password);
 
