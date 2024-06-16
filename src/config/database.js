13503003f408975e2eb95f3e9558644d37d03982
lcsAndrade <lucascandradex@protonmail.com>
2024-06-16 19:46:10 -0300

    fix: export database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-export const config = 
+const config = 
   process.env.NODE_ENV === "homolog" ? {
     POSTGRES_USER: "u203vrjb817tgm",
     POSTGRES_HOST: "c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
@@ -9,3 +9,6 @@ export const config =
     POSTGRES_DB: "",
     POSTGRES_PASSWORD: "",
   }
+
+
+module.exports = { config }
