f9c5c6fe70c2e4a0f9f18ef569e218c86a328903
lcsAndrade <lucascandradex@protonmail.com>
2024-06-16 19:52:14 -0300

    fix: verifica node env para configurar banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,26 @@
-const config = 
-  process.env.NODE_ENV === "homolog" ? {
+console.log(process.env);
+
+let config;
+if (process.env.NODE_ENV === "homolog") {
+  config = {
     POSTGRES_USER: "u203vrjb817tgm",
     POSTGRES_HOST: "c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
     POSTGRES_DB: "d6nofbvj2rl40d",
-  } : {
+  }
+} else if (process.env.NODE_ENV === "prod") {
+  config = {
     POSTGRES_USER: "",
     POSTGRES_HOST: "",
     POSTGRES_DB: "",
     POSTGRES_PASSWORD: "",
   }
+} else {
+  config = {
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+  }
+}
 
 
 module.exports = { config }
