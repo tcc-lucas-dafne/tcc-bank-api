b840b08a87560a9cf478e9007d072c9d5701850a
lcsAndrade <lucascandradex@protonmail.com>
2024-06-16 02:32:09 -0300

    feat: atualiza script sql

--- sql-scripts/create.sql
+++ sql-scripts/create.sql
@@ -3,15 +3,26 @@ CREATE TABLE account (
   name VARCHAR(64) NOT NULL,
   email VARCHAR(64) UNIQUE NOT NULL,
   password VARCHAR(64) NOT NULL,
+  image TEXT,
   
 	PRIMARY KEY (account_id)
 );
 
 
 CREATE TABLE account_detail (
-	account_id INT NOT NULL, 
-  balance numeric not null default 0.00,
-  acc_limit numeric NOT NULL,
+	account_id INT UNIQUE NOT NULL, 
+  balance NUMERIC NOT NULL DEFAULT 0.00,
+  acc_limit NUMERIC NOT NULL,
   
   CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
+
+CREATE TYPE account_request_status AS ENUM ('approved', 'reproved', 'pending');
+
+CREATE TABLE account_request (
+  account_id INT NOT NULL,
+  requested_amount NUMERIC NOT NULL,
+  request_date TIMESTAMPTZ NOT NULL,
+  review_date TIMESTAMPTZ,
+  status account_request_status NOT NULL DEFAULT,
 );
